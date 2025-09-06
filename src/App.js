b21bada9ce4fcf20ab071d7f771929e8ffbab587
chanmin <kimchanmin1@gmail.com>
2025-09-06 15:08:33 +0900

    chore: 마이너 수정사항

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header1">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
